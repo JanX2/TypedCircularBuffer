fdf2adf3a65fbc828f2aa9a9bf44f9f61e298d6a
Jan Weiß <jan@geheimwerk.de>
2021-10-30 18:37:48 +0200

    Add “testLargeAmountsOfItems” test.

--- TypedCircularBufferTests/TypedCircularBufferMultipleItemsTests.swift
+++ TypedCircularBufferTests/TypedCircularBufferMultipleItemsTests.swift
@@ -40,10 +40,40 @@ final class TypedCircularBufferMultipleItemsTests: XCTestCase {
 		XCTAssert(buffer.isEmpty)
 	}
 	
+	func testLargeAmountsOfItems() {
+		let count = 2 * Buffer<Int>.capacityGranularity - 100
+		let items = Array(0..<count)
+		
+		let buffer = Buffer<Int>(minimumCapacity: items.count)!
+		let capacity = buffer.capacity
+		XCTAssert(capacity >= count)
+		
+		buffer.push(items)
+		XCTAssertEqual(buffer.pop(amount: items.count), items)
+		XCTAssert(buffer.isEmpty)
+
+		// Fill over capacity.
+		XCTAssertEqual(buffer.push(items), items.count)
+		XCTAssert(buffer.count + items.count > capacity)
+		XCTAssertEqual(buffer.push(items), 0) // Test filling over capacity. Fails by not writing the items. Clears buffer.
+		XCTAssertEqual(buffer.count, 0)
+		
+		// Fill completely.
+		XCTAssertEqual(buffer.push(items), items.count)
+		let capacityRemaining = buffer.available
+		let partOfItems = Array(items[0..<capacityRemaining])
+		XCTAssertEqual(buffer.push(partOfItems), capacityRemaining)
+		XCTAssertEqual(buffer.pop(amount: items.count), items)
+		XCTAssertEqual(buffer.pop(amount: buffer.count), partOfItems)
+		
+		XCTAssert(buffer.isEmpty)
+	}
+	
 	static var allTests = [
 		("testPushMultipleItems", testPushMultipleItems),
 		("testPopMultipleItems", testPopMultipleItems),
 		("testPopMultipleItemsWithOverflow", testPopMultipleItemsWithOverflow),
+		("testLargeAmountsOfItems", testLargeAmountsOfItems),
 	]
 }
 
